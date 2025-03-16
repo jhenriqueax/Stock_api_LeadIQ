10d0c5fd2a2e0bbf4647c1e56083537e0d19d57d
Jhenriqueax <jhenriquealmeidaxavier@hotmail.com>
2025-03-16 18:45:19 -0300

    base

---
+++ src/test/java/com/example/Stock_api_LeadIQ/StockApiLeadIqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Stock_api_LeadIQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApiLeadIqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
