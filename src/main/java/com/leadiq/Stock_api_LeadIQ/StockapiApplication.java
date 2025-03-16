5ef06967a1ebb3352931cd12c48a053fe266b1ec
Jhenriqueax <jhenriquealmeidaxavier@hotmail.com>
2025-03-16 20:42:31 -0300

    Update StockapiApplication.java

--- src/main/java/com/leadiq/Stock_api_LeadIQ/StockapiApplication.java
+++ src/main/java/com/leadiq/Stock_api_LeadIQ/StockapiApplication.java
@@ -2,9 +2,8 @@ package com.leadiq.Stock_api_LeadIQ;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class StockApiApplication {
 
     public static void main(String[] args) {
