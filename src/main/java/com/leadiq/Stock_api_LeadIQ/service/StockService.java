2539f9f3fc2e2ec0872de0a7f42b02f12c950fdc
Jhenriqueax <jhenriquealmeidaxavier@hotmail.com>
2025-03-17 22:56:22 -0300

    exponencial backoff

--- src/main/java/com/leadiq/Stock_api_LeadIQ/service/StockService.java
+++ src/main/java/com/leadiq/Stock_api_LeadIQ/service/StockService.java
@@ -7,6 +7,9 @@ import com.leadiq.Stock_api_LeadIQ.repository.StockRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
@@ -20,8 +23,8 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
-
 @Service
+@EnableRetry
 public class StockService {
 
     private final StockRepository stockRepository;
@@ -35,47 +38,53 @@ public class StockService {
         this.restTemplate = new RestTemplate();
     }
 
+
+     @Retryable(
+        value = HttpClientErrorException.class, 
+        maxAttempts = 7, 
+        backoff = @Backoff(delay = 1000, multiplier = 2) // 1s → 2s → 4s → 8s → 16s → 32s → 64s
+    )
     public void fetchAndStoreStockData(String companySymbol, String fromDate, String toDate) {
         String url = String.format(
                 "https://api.polygon.io/v2/aggs/ticker/%s/range/1/day/%s/%s?apiKey=%s",
                 companySymbol, fromDate, toDate, polygonApiKey);
-    
+
         try {
             ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
             String responseBody = responseEntity.getBody();
-    
+
             ObjectMapper objectMapper = new ObjectMapper();
             PolygonResponseDTO polygonResponse = objectMapper.readValue(responseBody, PolygonResponseDTO.class);
-    
+
             if (!"OK".equals(polygonResponse.getStatus()) || polygonResponse.getResults() == null) {
                 throw new ApiException("Failed to fetch stock data from Polygon.io");
             }
-    
+
             List<Stock> stocks = new ArrayList<>();
-    
             for (PolygonStockDTO result : polygonResponse.getResults()) {
                 LocalDate date = Instant.ofEpochMilli(result.getTimestamp())
                                         .atZone(ZoneId.systemDefault())
                                         .toLocalDate();
-    
+
                 stocks.add(new Stock(null, companySymbol, date, 
                                     result.getOpenPrice(), result.getClosePrice(),
                                     result.getHighPrice(), result.getLowPrice(), result.getVolume()));
             }
-    
+
             stockRepository.saveAll(stocks);
-    
+
         } catch (HttpClientErrorException e) {
             if (e.getStatusCode() == HttpStatus.TOO_MANY_REQUESTS) {
-                throw new ApiException("Polygon API rate limit exceeded. Please try again later.");
+                System.out.println("API limit exceeded! Retrying...");
+                throw e; 
             }
-            throw new ApiException("Error fetching data: " + e.getMessage());
+            throw new ApiException("Error fetching data from Polygon API: " + e.getMessage());
         } catch (Exception e) {
             throw new ApiException("Unexpected error: " + e.getMessage());
         }
     }
-    
 
+    
     public StockDTO getStockBySymbolAndDate(String companySymbol, String dateStr) {
         LocalDate date = LocalDate.parse(dateStr.trim());
         Stock stock = stockRepository.findByCompanySymbolAndDate(companySymbol, date)
@@ -85,3 +94,4 @@ public class StockService {
                             stock.getClosePrice(), stock.getHighPrice(), stock.getLowPrice(), stock.getVolume());
         }
     }
+
